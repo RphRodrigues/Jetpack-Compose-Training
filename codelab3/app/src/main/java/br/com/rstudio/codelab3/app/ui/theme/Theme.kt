fda3b839357d63b0a8c2edd646b83a16cc963a35
Raphael <rodrigues.rph@gmail.com>
2025-05-17 21:43:01 -0300

    third compose codelab

--- codelab3/app/src/main/java/br/com/rstudio/codelab3/app/ui/theme/Theme.kt
+++ codelab3/app/src/main/java/br/com/rstudio/codelab3/app/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
-package br.com.rstudio.designsystem.ui.theme
+package br.com.rstudio.codelab3.app.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,6 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -33,32 +33,18 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
-//private val LightColorScheme1 = lightColorScheme(
-//  surface = Blue,
-//  onSurface = Color.White,
-//  primary = LightBlue,
-//  onPrimary = Navy
-//)
-//
-//private val DarkColorScheme1 = darkColorScheme(
-//  surface = Blue,
-//  onSurface = Navy,
-//  primary = Navy,
-//  onPrimary = Chartreuse
-//)
-
 @Composable
-fun DesignSystemTheme(
+fun ComposeTrainingTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
   dynamicColor: Boolean = true,
   content: @Composable () -> Unit
 ) {
   val colorScheme = when {
-//    dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//      val context = LocalContext.current
-//      if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//    }
+    dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+      val context = LocalContext.current
+      if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    }
 
     darkTheme -> DarkColorScheme
     else -> LightColorScheme
